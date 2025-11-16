bcc7d07b194ebc5c2afd1d2748dc85fe46b2d4b2
Andi <andi_haller@hotmail.com>
2025-11-15 21:47:15 -0300

    First commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    my_text_node = TextNode("Hellouuu", TextType.TEXT_LINK, "https://www.boot.dev")
+    print(my_text_node)
+
+if __name__ == "__main__":
+    main()
