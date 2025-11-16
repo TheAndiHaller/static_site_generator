a304ea84c3bfa8149e36156d23c2cad37d41b8b9
Andi <andi_haller@hotmail.com>
2025-11-15 21:53:52 -0300

    Tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,7 +7,11 @@ class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
+        node3 = TextNode("This is a text nod", TextType.BOLD)
+        node4 = TextNode("This is a text node", TextType.LINK)
         self.assertEqual(node, node2)
+        self.assertNotEqual(node, node3)
+        self.assertNotEqual(node, node4)
 
 
 if __name__ == "__main__":
